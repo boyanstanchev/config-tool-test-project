f1ebd9acb4a1045c5a7b439492075ee1f18c5d4b
BStanchevArena <bstanchev@arena.com>
2021-03-11 13:26:38 +0200

    Updated to the latest stable angular version

--- src/app/components/segment-form/segment-form.component.spec.ts
+++ src/app/components/segment-form/segment-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SegmentFormComponent } from './segment-form.component';
 
@@ -6,7 +6,7 @@ describe('SegmentFormComponent', () => {
   let component: SegmentFormComponent;
   let fixture: ComponentFixture<SegmentFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SegmentFormComponent ]
     })
--- src/app/components/segment-preview/segment-preview.component.spec.ts
+++ src/app/components/segment-preview/segment-preview.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SegmentPreviewComponent } from './segment-preview.component';
 
@@ -6,7 +6,7 @@ describe('SegmentPreviewComponent', () => {
   let component: SegmentPreviewComponent;
   let fixture: ComponentFixture<SegmentPreviewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SegmentPreviewComponent ]
     })
